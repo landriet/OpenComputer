8118aa09f89a160895725cf72d903c2563601eb2
Louis <louis.andriet@gmail.com>
2021-07-20 09:06:39 -0400

    remove modem dependency

--- fp/master.lua
+++ fp/master.lua
@@ -1,15 +1,15 @@
 local component = require("component")
 local patterns = require("fp/patterns")
 local rs = require("fp/rs")
-local modem = component.modem
+--local modem = component.modem
 
 local master = {}
 
 function master.run()
     print("Start")
-    modem.open(1)
+    --modem.open(1)
     rs.fetchAllItems(patterns.ENDER_PEARL.ingredients)
-    modem.broadcast(2, "ENDER_PEARL")
+    --modem.broadcast(2, "ENDER_PEARL")
 
 end
 
